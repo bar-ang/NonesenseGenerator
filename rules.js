4a7920b202bfdff92da9feb70effc14224db041a
Bar Angel <bar@lightbitslabs.com>
2019-02-27 18:29:36 +0200

    rules indentation

--- rules.js
+++ rules.js
@@ -1,53 +1,53 @@
 {
-        "SENTENCE" : [
-            ["IMPARATIVE_SENTENCE"],
-            ["IMPARATIVE_SENTENCE"],
-            ["IMPARATIVE_SENTENCE"],
-            ["QUESTION"]
-        ],
-        
-        "IMPARATIVE_SENTENCE" : [
-            ["SIMPLE_SENTENCE"],
-            ["SIMPLE_SENTENCE", "COMMA", "CONJUNCTION", "IMPARATIVE_SENTENCE"]
-        ],
+    "SENTENCE" : [
+        ["IMPARATIVE_SENTENCE"],
+        ["IMPARATIVE_SENTENCE"],
+        ["IMPARATIVE_SENTENCE"],
+        ["QUESTION"]
+    ],
+    
+    "IMPARATIVE_SENTENCE" : [
+        ["SIMPLE_SENTENCE"],
+        ["SIMPLE_SENTENCE", "COMMA", "CONJUNCTION", "IMPARATIVE_SENTENCE"]
+    ],
 
-        "QUESTION" : [
-            ["SIMPLE_QUESTION", "QUESTION_MARK"]
-        ],
+    "QUESTION" : [
+        ["SIMPLE_QUESTION", "QUESTION_MARK"]
+    ],
 
-        "SIMPLE_QUESTION" : [
-            ["QUESTION_VERB", "SUBJECT", "VERB_PHRASE"]
-        ],
+    "SIMPLE_QUESTION" : [
+        ["QUESTION_VERB", "SUBJECT", "VERB_PHRASE"]
+    ],
 
-        "SIMPLE_SENTENCE" : [
-            ["SUBJECT", "VERB_PHRASE"]
-        ],
+    "SIMPLE_SENTENCE" : [
+        ["SUBJECT", "VERB_PHRASE"]
+    ],
 
-        "VERB_PHRASE" : [
-            ["VERB"],
-            ["NOT_CLAUSE", "VERB"]
-        ],
+    "VERB_PHRASE" : [
+        ["VERB"],
+        ["NOT_CLAUSE", "VERB"]
+    ],
 
-        "VERB" : [
-            ["TRANSITIVE_VERB", "OBJECT"],
-            ["INTRANSITIVE_VERB"],
-            ["TRANSITIVE_VERB", "OBJECT", "ADVERB"],
-            ["INTRANSITIVE_VERB", "ADVERB"],
-            ["ADVERB", "TRANSITIVE_VERB", "OBJECT"]
-        ],
+    "VERB" : [
+        ["TRANSITIVE_VERB", "OBJECT"],
+        ["INTRANSITIVE_VERB"],
+        ["TRANSITIVE_VERB", "OBJECT", "ADVERB"],
+        ["INTRANSITIVE_VERB", "ADVERB"],
+        ["ADVERB", "TRANSITIVE_VERB", "OBJECT"]
+    ],
 
-        "SUBJECT" : [
-            ["NOUN_PHRASE"],
-            ["PRONOUN"]
-        ],
+    "SUBJECT" : [
+        ["NOUN_PHRASE"],
+        ["PRONOUN"]
+    ],
 
-        "OBJECT" : [
-            ["NOUN_PHRASE"]
-        ],
+    "OBJECT" : [
+        ["NOUN_PHRASE"]
+    ],
 
-        "NOUN_PHRASE" : [
-            ["ARTICLE", "NOUN"],
-            ["ARTICLE", "ADJECTIVE", "NOUN"]
-        ]
+    "NOUN_PHRASE" : [
+        ["ARTICLE", "NOUN"],
+        ["ARTICLE", "ADJECTIVE", "NOUN"]
+    ]
 }
 
